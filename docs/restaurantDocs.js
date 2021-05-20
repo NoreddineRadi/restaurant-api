659e341d7f786a987a669003c3982dcdacc81a95
rano <rano@rtbf.be>
2021-05-20 16:24:58 +0200

    fix(): required params locations and raduis in swagger

--- docs/restaurantDocs.js
+++ docs/restaurantDocs.js
@@ -4,9 +4,9 @@ module.exports = {
 			tags: ['Restaurants'],
 			summary: 'Get restaurants',
 			parameters: [
-				{ name: 'latitude', in: 'query', type: 'number', format: 'float', /* required: true, */ default: 50.670139},
-				{ name: 'longitude', in: 'query', type: 'number',  format: 'float', /* required: true, */ default: 4.615537 },
-				{ name: 'radius', in: 'query', type: 'number', description: 'the searching radius in (m)', default: 1000 },
+				{ name: 'latitude', in: 'query', type: 'number', format: 'float', required: true, default: 50.670139},
+				{ name: 'longitude', in: 'query', type: 'number',  format: 'float', required: true, default: 4.615537 },
+				{ name: 'radius', in: 'query', type: 'number', required: true, description: 'the searching radius in (m)', default: 1000 },
 				{ name: 'rangePrice', in: 'query', type: 'number', enum: [0,1,2,3,4,], description: 'Max: 0, 1, 2, 3 or 4',  },
 			  { name: 'attendees', in: 'query', type: 'array', items: {type: 'string', default: ''}, minItems:1},
 			],
