bcb89e3409f3f663d09d890cd6411ca0d7028f68
rano <rano@rtbf.be>
2021-07-14 14:46:22 +0200

    display token

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const axios = require('axios')
-
+const jwt = require('jsonwebtoken')
+const secret = Buffer.from(process.env.JWT_SUPER_SECRET, 'base64')
+const token = jwt.sign({name: 'rano', id: '10000'}, secret, {
+  expiresIn: '24h',
+})
+console.log(token)
 router.get('/', async function(req, res, next) {
   const {latitude, longitude, radius, rangePrice, attendees} = req.query
   const baseUrl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
@@ -14,13 +19,13 @@ router.get('/', async function(req, res, next) {
   if (!latitude || !longitude || !radius || isNaN(latitude) || isNaN(longitude) || isNaN(radius)) {
     res.status(400).send({
       success: false,
-      message: 'the params `latitude, longitude and radius` is required and it should be a number',
+      message: 'The params `latitude, longitude and radius` is required and it should be a number',
       data: []
     })
   } else if ((rangePrice && isNaN(rangePrice) ) || (rangePrice < 0 || rangePrice > 4)) {
     res.status(400).send({
       success: false,
-      message: 'the range price it is a number and it should be 0,1,2,3 or 4',
+      message: 'Range price is a number and it should be 0,1,2,3 or 4',
       data: []
     })
   } else {
